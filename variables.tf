823baf8bd27a5b4870b56024673ed7e267d1312b
ganes891 <ganesan.kandasami@gmail.com>
2024-08-14 15:39:01 +0800

    new changes on TF files

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "prefix_separator" {
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = ""
+  default     = "ctc-dev"
 }
 
 variable "cluster_version" {
