84510317d52ed295c27aa5bb05d2f312dd135ddf
ganes891 <ganesan.kandasami@gmail.com>
2024-08-22 16:54:00 +0800

    new changes on TF files

--- eks-admin-tf/01-ekscluster-terraform-manifests/c1-versions.tf
+++ eks-admin-tf/01-ekscluster-terraform-manifests/c1-versions.tf
@@ -9,14 +9,14 @@ terraform {
      }
   }
   # Adding Backend as S3 for Remote State Storage
-  backend "s3" {
-    bucket = "terraform-on-aws-eks"
-    key    = "dev/eks-cluster/terraform.tfstate"
-    region = "ap-southeast-1" 
+  #backend "s3" {
+   # bucket = "terraform-on-aws-eks"
+   # key    = "dev/eks-cluster/terraform.tfstate"
+   # region = "ap-southeast-1" 
  
     # For State Locking
-    dynamodb_table = "dev-ekscluster"    
-  }  
+   # dynamodb_table = "dev-ekscluster"    
+  #}  
 }
 
 # Terraform Provider Block
